1b6b303e78c837ebf6c9ef9af1738dbe4d0f2640
maxwell kiplagat <kiplagat@kali>
2026-04-15 16:36:45 +0100

    used the module, f string, and the if/else condtion to check if a number is odd or even.

---
+++ day_two/evenodd.py
@@ -0,0 +1,6 @@
+number_to_check = int(input('please enter the number to check if it is odd or even?'))
+if (number_to_check%2 != 0):{
+    print(f'{number_to_check} is an odd number!')
+}
+else:
+    print(f'{number_to_check} is an even number!')
