8680304595d7c2fea24d8f422f9e801a82ee0bfc
maxwell kiplagat <kiplagat@kali>
2026-04-16 10:27:47 +0100

    added a simple code about tip calculator

--- day_one/tipcalculator.py
+++ day_one/tipcalculator.py
@@ -1,5 +1,7 @@
 bill = input('Welcome to the tip calculator!\n' 'what is the the total bill?')
 tip = input('How much tip would you like to give? 10,12 or 15?')
 people = input('How many people to spilt the bill?')
+# basically tip is calculated by multiplying the bill by the percentage of the tip and the adding the tip to the bill and then dividing by the number of people to get the total per person   
 total =  (float(bill) * ((float(tip)+100)/100))/int(people)
+#here we convert the total to a two decimal place using the .2f format specifier in the f-string
 print(f'Each person should pay: ${total:.2f}')
