209e637c83874684d4d913476502f1b405288013
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2026-04-17 12:05:39 +0100

    this is a code that usually run behind the sceness  when you use max()

---
+++ day_five/highestscore.py
@@ -0,0 +1,6 @@
+student_scores = [180,124,165,173,189,169,146]
+highest_number = 0
+for large in student_scores:
+    if large > highest_number:
+        highest_number = large
+print(highest_number)
