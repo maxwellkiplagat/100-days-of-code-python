2821a9b2cd7e207628e4579796e082e8592a1a0d
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2026-04-18 09:26:35 +0100

    added the comments to separate the two codes

--- day_five/passwordgenerator.py
+++ day_five/passwordgenerator.py
@@ -5,6 +5,33 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+random_chosen_password = []
+
+print('welcome to the pyPassword Generator!')
+
+nr_letters = int(input('How many letters would you like in your password?\n'))
+random_letters = random.sample(letters,nr_letters)
+random_chosen_password.append(random_letters)
+
+nr_symbol = int(input(f'How many symbols would you like?\n'))
+random_symbol = random.sample(symbols,nr_symbol)
+random_chosen_password.append(random_symbol)
+
+
+
+nr_number = int(input(f'How many numbers would you like?\n'))
+random_number = random.sample(numbers,nr_number)
+random_chosen_password.append(random_number)
+
+
+
+
+flat = list(itertools.chain.from_iterable(random_chosen_password))
+random.shuffle(flat)
+
+nal = ''.join(flat)
+
+print(nal)
 
 
 
@@ -25,4 +52,4 @@ for char in range(0, nr_number):
 finall_ans = list(password)
 random.shuffle(finall_ans)
 final_password = ''.join(finall_ans)
-print(f'Your password is: {final_password}')
+print(final_password)
